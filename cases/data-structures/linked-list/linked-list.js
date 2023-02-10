d180481203ce7721e08b3de5f5bd118ee2e5fde9
ivan <ivan.kavtarev@mail.ru>
2023-02-10 23:33:13 +0300

    linked list

---
+++ cases/data-structures/linked-list/linked-list.js
@@ -0,0 +1,67 @@
+class LinkedList {
+  head = null;
+
+  addNode(node) {
+    node.next = this.head;
+    this.head = node;
+  }
+
+  length() {
+    let i = 1;
+    let temp = this.head;
+
+    while (temp.next) {
+      i += 1;
+      temp = temp.next
+    }
+    
+    return i;
+  }
+
+  insert(node, index) {
+    let temp = this.head;
+
+    if (temp === null) {
+      this.head = node;
+    }
+
+    while (index > 1 && temp.next !== null) {
+      temp = temp.next
+      index -= 1;
+    }
+
+    node.next = temp.next;
+    temp.next = node;
+  }
+
+  printData() {
+    let temp = this.head;
+
+    while(temp !== null) {
+      console.log(temp.data);
+      temp = temp.next;
+    }
+  }
+}
+
+class LinkedListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let oneWayList = new LinkedList();
+
+const first = new LinkedListNode(1);
+const second = new LinkedListNode(2);
+const third = new LinkedListNode(3);
+const fourth = new LinkedListNode(4);
+
+oneWayList.addNode(first);
+oneWayList.addNode(second);
+oneWayList.addNode(third);
+
+
+oneWayList.insert(fourth, 1);
+oneWayList.printData()
